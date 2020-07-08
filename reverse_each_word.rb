32baf14283f38f7802838fccfb273317ad1d0824
Mimi Ojserkis <mimioj@gmail.com>
2020-07-08 18:00:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(array)
+def reverse_each_word(sentence)
   separated_array = sentence.split(" ")
   reversed_array = separated_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
